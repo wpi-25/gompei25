eea8f1b89345d2754724ad69b2ffca783618e169
Cara Salter <cara@devcara.com>
2021-09-02 16:15:12 -0400

    Implement RO redis interface

--- src/util/leveling.rs
+++ src/util/leveling.rs
@@ -1,4 +1,11 @@
+use chrono::prelude::*;
 
+pub struct LevelData {
+    pub msg_count: u32,
+    pub xp: u32,
+    pub level: u32,
+    pub last_msg: chrono::DateTime<Utc>,
+}
 
 fn get_level_number(xp: u32) -> u32 {
     let xp = xp as f64;
@@ -13,6 +20,24 @@ fn get_level_number(xp: u32) -> u32 {
 
 fn get_level_cost(level: u64) -> u64 {
     (level.pow(3) * 50).into()
+}
+
+pub async fn get_user_level(user_id: u64, mut redis_conn: redis::aio::Connection) -> Result<LevelData, Box<dyn std::error::Error>> {
+    let msg_count = redis::cmd("GET").arg(&[format!("{}:count", &user_id)]).query_async(&mut redis_conn).await?; 
+    let xp: u32 = redis::cmd("GET").arg(&[format!("{}:exp", &user_id)]).query_async(&mut redis_conn).await?;
+    let last_msg_num: i64 = redis::cmd("GET").arg(&[format!("{}:last", &user_id)]).query_async(&mut redis_conn).await?;
+
+    let last_msg = Utc.timestamp(last_msg_num, 0);
+    let level = get_level_number(xp.into());
+
+    let level_data = LevelData {
+        msg_count,
+        xp,
+        last_msg,
+        level,
+    };
+        
+    Ok(level_data)
 }
 
 #[cfg(test)]
