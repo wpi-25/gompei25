a78ee9174409f01662f6e492b2dca04b1e626ec2
Sam <centersam19@gmail.com>
2021-09-10 16:05:11 -0400

    Create help command

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -1,10 +1,41 @@
-use serenity::framework::standard::{macros::command, CommandResult};
+use std::{
+    collections::HashSet
+};
+
+use serenity::framework::standard::{
+    help_commands,
+    macros::{command, help},
+    Args,
+    CommandGroup,
+    CommandResult,
+    HelpOptions
+};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.channel_id.say(&ctx.http, "Pong!").await?;
+
+    Ok(())
+}
+
+#[help]
+#[individual_command_tip = "If you want more information about a specific command, pass it as an argument"]
+#[command_not_found_text = "Could not find `{}`"]
+#[max_levenshtein_distance(3)]
+#[lacking_permissions = "Hide"]
+#[lacking_role = "Nothing"]
+#[wrong_channel = "Strike"]
+async fn help(
+    ctx: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    let _ = help_commands::with_embeds(ctx, msg, args, help_options, groups, owners).await;
 
     Ok(())
 }
